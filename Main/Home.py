709095f3083b010ec278d763730a484b012e5d27
MonkeyBHands <gavin.tracey@gmail.com>
2025-09-16 22:00:09 +0100

    Home page

--- Main/Home.py
+++ Main/Home.py
@@ -8,10 +8,11 @@ st.set_page_config(
 )
 
 st.title('Welcome to the Glory Hole')
+st.write('Use the pages on the left')
 st.image('data/JJGH.jpeg', width=500)
 
 st.sidebar.write("Brought to you by:")
-st.sidebar.image('JJGH_Advanced/data/JJGH.jpeg', width=300)
+st.sidebar.image('data/JJGH.jpeg', width=300)
 
 st.write("All data provided by https://github.com/nflverse/nflverse-data/releases")
 st.write("Brought to you by JJGH - There ain't no glory hole like a Jerry Jones Glory Hole!")
